80979fe74411ecbde312aa513a405fd91c44d4db
HEUMMAN <69341986+HEUMMAN@users.noreply.github.com>
2021-12-21 11:34:37 +0900

    Update Mode Select
    Cruise 모드 진입
    ∨ 현재 차속이 50km이상일때, Cruise 버튼을 누르면 Cruise 모드에 진입 한다. (On 상태)
    ∨ Cruise On 상태에 진입하면 현재 차속을 목표 차속으로 유지 한다.
    
    Cruise 목표 차속 저장
    X Cruise On 상태에에서 목표 차속이 변경되면 변경 된 목표 차속을 저장한다.
    
    Cruise 모드 해제
    ∨ 목표 차속이 설정된 상태에서 CANCEL 버튼이 입력되면 Cruise 모드가 해제된다.
    ∨ 목표 차속이 설정된 상태에서 브레이크가 입력되면 Cruise 모드가 해제된다.
    ∨ Cancel 버튼 동작으로 Cruise 모드가 해제되면 저장된 목표 차속은 초기화(-1) 된다.

--- Project1/main.c
+++ Project1/main.c
@@ -1,4 +1,30 @@
 #include <stdio.h>
+
+//cruise_mode_status에서 cruise mode on은 1, cruise mode off는 0
+int cruise_mode(float current_car_speed, float *target_car_speed, char car_break, char cancel_button, char *cruise_mode_status, char cruise_button){
+	float saved_target_car_speed = 0;
+
+	if (cancel_button == 1 && *target_car_speed != -1) {
+		*cruise_mode_status = 0;
+		*target_car_speed = -1;
+		return 0;
+	}
+
+	if (car_break == 1 && *target_car_speed != -1) {
+		*cruise_mode_status = 0;
+		return 0;
+	}
+
+	if (cruise_button == 1 && current_car_speed >= 50) {
+		*cruise_mode_status = 1;
+	}
+
+	if (*cruise_mode_status == 1) {
+		current_car_speed = *target_car_speed;
+	}
+	return 0;
+}
+
 int main() {
 	printf("Hello World!");
 
